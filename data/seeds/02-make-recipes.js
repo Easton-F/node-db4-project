de92e3d489eacf953da7f2f12974a17292bf1c38
Easton <easton.fritsch21@gmail.com>
2023-10-02 17:01:56 -0600

    wip breakpoint

--- data/seeds/02-make-recipes.js
+++ data/seeds/02-make-recipes.js
@@ -1,3 +1,20 @@
+const recipes = [
+    { recipe_name: 'Broccoli Pesto Pasta' },
+    { recipe_name: 'Lemon Chicken' },
+    { recipe_name: 'Salmon en Papillote' },
+]
+
+const ingredients = [
+    { ingredient_name: 'Broccoli', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pesto', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pasta', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Lemon', ingredient_unit: 'slices' },
+    { ingredient_name: 'Chicken', ingredient_unit: 'kil;os' },
+    { ingredient_name: 'Salmon', ingredient_unit: 'grams' },
+]
+
+
+
 exports.seed = function (knex) {
-    
+
 }
